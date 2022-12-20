25b305eca59fb7f34db283bf86d0080a7b784348
areyouontheway <mandal.gulshan2003@gmail.com>
2022-12-20 02:01:48 -0800

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // name has private access in com.driver.RWOnly
+        //obj.name= "phani"
+        obj.setname("harsh");
+        obj.getname();
+    }
   
 }
